923e8d3eec929278f85a815b6ee6d58d8dfc8091
James <jeeaton001@gmail.com>
2019-11-01 12:50:49 -0600

    hi

--- PA3/simulation_3.py
+++ PA3/simulation_3.py
@@ -3,8 +3,8 @@ Created on Oct 12, 2016
 
 @author: mwittie
 '''
-import network_2 as network
-import link_2 as link
+import network_3 as network
+import link_3 as link
 import threading
 from time import sleep
 
@@ -16,12 +16,22 @@ if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
     
     #create network nodes
-    client = network.Host(1)
-    object_L.append(client)
-    server = network.Host(2)
-    object_L.append(server)
+    host_1 = network.Host(1)
+    object_L.append(host_1)
+    host_3 = network.Host(3)
+    object_L.append(host_3)
+    host_2 = network.Host(2)
+    object_L.append(host_2)
+    host_4 = network.Host(4)
+    object_L.append(host_4)
     router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
+    router_b = network.Router(name='B', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_b)
+    router_c = network.Router(name='C', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_c)
+    router_d = network.Router(name='D', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_d)
     
     #create a Link Layer to keep track of links between network nodes
     link_layer = link.LinkLayer()
@@ -29,15 +39,24 @@ if __name__ == '__main__':
     
     #add all the links
     #link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
-    link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link.Link(router_a, 0, server, 0, 30))
+    link_layer.add_link(link.Link(host_1, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(host_2, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(router_a, 0, router_b, 0, 30))
+    link_layer.add_link(link.Link(router_a, 0, router_c, 0, 30))
+    link_layer.add_link(link.Link(router_b, 0, router_d, 0, 30))
+    link_layer.add_link(link.Link(router_c, 0, router_d, 0, 30))
+    link_layer.add_link(link.Link(router_d, 0, host_3, 0, 30))
+    link_layer.add_link(link.Link(router_d, 0, host_4, 0, 30))
     
     
     #start all the objects
     thread_L = []
-    thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
-    thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
+    thread_L.append(threading.Thread(name=host_1.__str__(), target=host_1.run))
+    thread_L.append(threading.Thread(name=host_3.__str__(), target=host_3.run))
     thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
     
     thread_L.append(threading.Thread(name="Network", target=link_layer.run))
     
@@ -49,10 +68,10 @@ if __name__ == '__main__':
     
     #create some send events
     '''for i in range(3):
-        client.udt_send(2, 'Sample data %d' % i)'''
+        host_1.udt_send(2, 'Sample data %d' % i)'''
 
     for i in range(2):
-        client.udt_send(2, host2Messages[i])
+        host_1.udt_send(2, host2Messages[i])
     
     
     #give the network sufficient time to transfer all packets before quitting
